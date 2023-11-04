4bcc77006b08c0a29151027eeb3b6d330575b620
APEbbers <paul.ebbers@gmail.com>
2023-11-04 14:10:44 +0100

    Issue with updating total NoSheets fixed

--- FillTitleBlock.py
+++ FillTitleBlock.py
@@ -39,7 +39,6 @@ import FillSpreadsheet
 
 def FillTitleBlock():
     from Settings import ENABLE_DEBUG
-    from Settings import MAP_NOSHEETS
     from Settings import USE_EXTERNAL_SOURCE
 
     # Fill the spreadsheet again for the latest update
@@ -78,20 +77,6 @@ def FillTitleBlock():
                         # check if there is a value. If there is an value, fill in.
                         str(sheet.get("B" + str(RowNum)))
 
-                        # # check if this is a field for the total number of sheets
-                        # PropertyName = str(sheet.get("A" + str(RowNum)))
-                        # # if ENABLE_DEBUG is True:
-                        # print(
-                        #     f"Map sheets is: {MAP_NOSHEETS} and propertyName is {PropertyName}"
-                        # )
-                        # if str(PropertyName).startswith("'"):
-                        #     PropertyName = str(PropertyName)[1:]
-                        # if MAP_NOSHEETS == PropertyName:
-                        #     FillSpreadsheet.MapData(sheet=sheet, MapSpecific=4)
-                        #     sheet.recompute()
-                        #     texts[str(sheet.get("A" + str(RowNum)))] = str(
-                        #         (int(sheet.get("B" + str(RowNum))))
-                        #     )
                     except Exception:
                         pass
                     else:

f99f6b1301abfe24562a9a2278c9ac84daea9081
APEbbers <paul.ebbers@gmail.com>
2023-12-31 22:55:28 +0100

    GetInt changed to GetFloat for AutoFit

--- Settings.py
+++ Settings.py
@@ -50,6 +50,13 @@ def GetIntSetting(settingName: str) -> int:
     return result
 
 
+def GetFloatSetting(settingName: str) -> int:
+    result = preferences.GetFloat(settingName)
+    if result == "":
+        result = None
+    return result
+
+
 def GetBoolSetting(settingName: str) -> bool:
     result = preferences.GetBool(settingName)
     if str(result).lower() == "none":
@@ -163,7 +170,7 @@ SPREADSHEET_COLUMNFONTSTYLE_ITALIC = GetBoolSetting("SpreadsheetColumnFontStyle_
 SPREADSHEET_COLUMNFONTSTYLE_UNDERLINE = GetBoolSetting(
     "SpreadsheetColumnFontStyle_Underline"
 )
-AUTOFIT_FACTOR = GetIntSetting("AutoFitFactor")
+AUTOFIT_FACTOR = GetFloatSetting("AutoFitFactor")
 
 # Enable debug mode. This will enable additional report messages
 ENABLE_DEBUG = GetBoolSetting("EnableDebug")

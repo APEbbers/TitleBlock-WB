cffb7fc69a3a58995b4ddd60e99feb157d46d3e1
APEbbers <paul.ebbers@gmail.com>
2024-01-12 18:56:45 +0100

    Bugfixes - 19n

--- FillSpreadsheet.py
+++ FillSpreadsheet.py
@@ -1055,14 +1055,6 @@ def ImportDataFreeCAD():
                         str(ExtSheet.getContents(Column4 + str(RowNumber))),
                     )
 
-                # # Check if the next row of the spreadsheet has data. If not this is the end of all the available values.
-                # try:
-                #     test = sheet.getContents("A" + str(RowNumber))
-                #     if test == "" or test is None:
-                #         break
-                # except Exception:
-                #     break
-
             # Finally recompute the document
             App.ActiveDocument.recompute(None, True, True)
 
